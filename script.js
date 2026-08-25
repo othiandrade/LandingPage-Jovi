01ae6e3de855ef6ac83fe94cd738ed88607402cf
Thiago Andrade <Thiagoandrade7@yahoo.com>
2026-08-25 03:06:36 -0300

    feat: adiciona interatividade ao menu/form, fonte local e ícones

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const menuLista = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function() {
     menuLista.classList.toggle('menu-aberto');
 });
+
+const linksMenu = document.querySelectorAll('nav ul a');
+
+linksMenu.forEach(function(link) {
+    link.addEventListener('click', function() {
+        menuLista.classList.remove('menu-aberto');
+    });
+});
+
+const formulario = document.querySelector('form');
+const mensagemSucesso = document.querySelector('#mensagemSucesso');
+
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+    mensagemSucesso.classList.add('visivel');
+    formulario.reset();
+});
